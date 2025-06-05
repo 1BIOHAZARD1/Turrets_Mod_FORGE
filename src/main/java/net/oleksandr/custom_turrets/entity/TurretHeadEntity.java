eeba66cc3643d4d712574926712bc49ea250c522
Doctor_K <78sashablade@gmail.com>
2025-06-05 22:57:12 +0300

    Updated position of turretEntity

--- src/main/java/net/oleksandr/custom_turrets/entity/TurretHeadEntity.java
+++ src/main/java/net/oleksandr/custom_turrets/entity/TurretHeadEntity.java
@@ -7,21 +7,40 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
-
+import net.minecraft.world.phys.Vec3;
 
 public class TurretHeadEntity extends Entity {
+
+    private BlockPos basePos;
+
     public TurretHeadEntity(EntityType<? extends TurretHeadEntity> type, Level level) {
         super(type, level);
+        this.noPhysics = true; // Щоб не падала та не штовхалась
+    }
+
+    public void setBasePos(BlockPos basePos) {
+        this.basePos = basePos;
+        this.setPos(basePos.getX() + 0.5, basePos.getY() + 1.0, basePos.getZ() + 0.5);
     }
 
     @Override
     protected void defineSynchedData() {}
 
     @Override
-    protected void readAdditionalSaveData(CompoundTag tag) {}
+    protected void readAdditionalSaveData(CompoundTag tag) {
+        if (tag.contains("BaseX")) {
+            basePos = new BlockPos(tag.getInt("BaseX"), tag.getInt("BaseY"), tag.getInt("BaseZ"));
+        }
+    }
 
     @Override
-    protected void addAdditionalSaveData(CompoundTag tag) {}
+    protected void addAdditionalSaveData(CompoundTag tag) {
+        if (basePos != null) {
+            tag.putInt("BaseX", basePos.getX());
+            tag.putInt("BaseY", basePos.getY());
+            tag.putInt("BaseZ", basePos.getZ());
+        }
+    }
 
     @Override
     public void tick() {
@@ -29,6 +48,12 @@ public class TurretHeadEntity extends Entity {
 
         if (this.level().isClientSide) return;
 
+        // Постійно "прилипати" до позиції бази
+        if (basePos != null) {
+            this.setPos(basePos.getX() + 0.5, basePos.getY() + 1.0, basePos.getZ() + 0.5);
+        }
+
+        // Проста логіка наведення на ціль
         LivingEntity target = this.level().getEntitiesOfClass(LivingEntity.class, this.getBoundingBox().inflate(8.0),
                         e -> e != null && e.isAlive() && !e.getType().equals(this.getType()))
                 .stream().findFirst().orElse(null);
@@ -37,9 +62,6 @@ public class TurretHeadEntity extends Entity {
             this.lookAt(target);
         }
     }
-
-
-
 
     private void lookAt(LivingEntity target) {
         double dx = target.getX() - this.getX();
@@ -55,5 +77,4 @@ public class TurretHeadEntity extends Entity {
         this.yRotO = yaw;
         this.xRotO = pitch;
     }
-
 }
